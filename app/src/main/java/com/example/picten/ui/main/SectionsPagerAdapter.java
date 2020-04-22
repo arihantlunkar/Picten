629938ea2cfd5c91ccabfe0d32814d793e71e706
Arihant Lunkar <arihant-cse15@snu.edu.in>
2020-04-23 01:16:40 +0530

    Bottom navigation added

--- app/src/main/java/com/example/picten/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/picten/ui/main/SectionsPagerAdapter.java
@@ -15,7 +15,7 @@ import com.example.picten.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[] {R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[] {R.string.movies_text, R.string.tv_shows_text};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
